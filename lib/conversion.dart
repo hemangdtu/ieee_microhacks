739535dd2a55dc3521d250da05efd72664b94a8a
hemangdtu <hemangdtu@gmail.com>
2020-12-25 03:00:52 +0530

    Added Dropdown List on Conversion screen

--- lib/conversion.dart
+++ lib/conversion.dart
@@ -11,6 +11,8 @@ class ConversionClass extends StatefulWidget {
 class _ConversionClassState extends State<ConversionClass> {
   TextEditingController _iptText = TextEditingController();
   double res = 0;
+  int _value = 1;
+  int _value2 = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +33,95 @@ class _ConversionClassState extends State<ConversionClass> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(7.0),
+                        child: DropdownButton(
+                            value: _value2,
+                            items: [
+                              DropdownMenuItem(
+                                child: Text(
+                                  "First Item",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                value: 1,
+                              ),
+                              DropdownMenuItem(
+                                child: Text(
+                                  "Second Item",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                value: 2,
+                              ),
+                              DropdownMenuItem(
+                                child: Text(
+                                  "Third Item",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                value: 3,
+                              ),
+                              DropdownMenuItem(
+                                child: Text(
+                                  "Fourth Item",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                value: 4,
+                              )
+                            ],
+                            onChanged: (value2) {
+                              setState(() {
+                                _value2 = value2;
+                              });
+                            }),
+                      ),
+                      Text(
+                        "to",
+                        style: TextStyle(fontSize: 25.0),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(7.0),
+                        child: DropdownButton(
+                            value: _value,
+                            items: [
+                              DropdownMenuItem(
+                                child: Text(
+                                  "First Item",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                value: 1,
+                              ),
+                              DropdownMenuItem(
+                                child: Text(
+                                  "Second Item",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                value: 2,
+                              ),
+                              DropdownMenuItem(
+                                child: Text(
+                                  "Third Item",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                value: 3,
+                              ),
+                              DropdownMenuItem(
+                                child: Text(
+                                  "Fourth Item",
+                                  style: TextStyle(fontSize: 20.0),
+                                ),
+                                value: 4,
+                              )
+                            ],
+                            onChanged: (value) {
+                              setState(() {
+                                _value = value;
+                              });
+                            }),
+                      )
+                    ],
+                  ),
                   Padding(
                     padding: const EdgeInsets.only(
                         left: 20.0, right: 20.0, bottom: 10.0),
@@ -43,20 +134,21 @@ class _ConversionClassState extends State<ConversionClass> {
                       ),
                       cursorColor: Colors.black,
                       decoration: InputDecoration(
-                          filled: true,
-                          //focusColor: Colors.red,
-                          fillColor: Colors.black12,
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(30.0),
-                              bottomLeft: Radius.circular(30.0),
-                              //bottomRight: Radius.circular(30.0),
-                              //topLeft: Radius.circular(30.0),
-                            ),
+                        filled: true,
+                        //focusColor: Colors.red,
+                        fillColor: Colors.black12,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(30.0),
+                            bottomLeft: Radius.circular(30.0),
+                            //bottomRight: Radius.circular(30.0),
+                            //topLeft: Radius.circular(30.0),
                           ),
-                          labelText: "Input Value",
-                          focusedBorder: InputBorder.none,
-                          labelStyle: TextStyle(color: Colors.white)),
+                        ),
+                        labelText: "Input Value",
+                        focusedBorder: InputBorder.none,
+                        labelStyle: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
                   RaisedButton(
